2302983521cde9a5362e94cd75610b7d51bd4212
db-0 <dan@dbsphere.net>
2025-05-02 18:07:37 -0600

    LeafNode and tests added

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,6 +9,8 @@ class HTMLNode():
         raise NotImplemented("not implemented yet")
     
     def props_to_html(self):
+        if not self.props:
+            return ""
         props_text = "".join(f" {k}=\"{v}\"" for k, v in self.props.items())
         return props_text
     
@@ -17,3 +19,15 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("all leaf nodes must have a value")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
